7ecdaa829db57f0977f8658b8bda7cfc684ba24a
Ollie Bourne <ollie.bourne@theappbusiness.com>
2020-02-24 12:31:45 +0000

    add warning and support for limited runs

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This script will remove the saved tracks from albums where every track of the al
 
 In case you need to reverse any changes at the end the script prints out all album ids that were updated.
 
+## Warning
+
+I've run this on my own Spotify account and am assuming that the saved album behaviour is the same for other users. I can't find an documentation on them adding this feature so I'm relying on what I've observed from my account. I would recommend starting with caution and running with `BATCH_LIMIT=1` and `DRY_RUN=true` env vars before doing a full run.
+
 ## Running the script
 
 You will need to create a OAuth token with `user-library-modify` permissions. [You can generate the token here](https://developer.spotify.com/console/delete-current-user-saved-tracks/).
@@ -15,4 +19,8 @@ npm install
 SPOTIFY_TOKEN=your-token node spotify-track-limit-saver.js
 ```
 
+### Dry and limited runs
+
 If you want to do a dry run first then you can set `DRY_RUN=true` as an env var and it will skip removing the tracks and just log out instead.
+
+If you just want to run on a small set of albums rather than your full collection you can use the `BATCH_LIMIT=1` env var. This will fetch and update 1 batch of 5 albums and then stop. The data for one album is quite large which is why the batching exists.
--- spotify-track-limit-saver.js
+++ spotify-track-limit-saver.js
@@ -3,127 +3,129 @@ const querystring = require("querystring");
 
 const TOKEN = process.env.SPOTIFY_TOKEN;
 if (!TOKEN) {
-	console.log(
-		"You need to pass an OAuth token with user-library-modify permissions as SPOTIFY_TOKEN env var. You can generate one for your account here: https://developer.spotify.com/console/delete-current-user-saved-tracks/"
-	);
-	return;
+  console.log(
+    "You need to pass an OAuth token with user-library-modify permissions as SPOTIFY_TOKEN env var. You can generate one for your account here: https://developer.spotify.com/console/delete-current-user-saved-tracks/"
+  );
+  return;
 }
 const DRY_RUN = Boolean(process.env.DRY_RUN);
 const ALBUMS_BATCH_SIZE = 5;
-const BATCH_LIMIT = 500;
+const BATCH_LIMIT = process.env.BATCH_LIMIT
+  ? Number(process.env.BATCH_LIMIT)
+  : 500;
 
 const updatedAlbumIds = [];
 
 const request = async options => {
-	const response = await axios({
-		method: "get",
-		headers: {
-			Authorization: `Bearer ${TOKEN}`
-		},
-		...options
-	});
-	return response.data;
+  const response = await axios({
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${TOKEN}`
+    },
+    ...options
+  });
+  return response.data;
 };
 
 const checkTracksSaved = async ids => {
-	const tracksSavedStatus = await request({
-		url: "https://api.spotify.com/v1/me/tracks/contains",
-		params: { ids: ids.join(",") }
-	});
-	const isStatusTrue = status => status === true;
+  const tracksSavedStatus = await request({
+    url: "https://api.spotify.com/v1/me/tracks/contains",
+    params: { ids: ids.join(",") }
+  });
+  const isStatusTrue = status => status === true;
 
-	const allTracksSaved = tracksSavedStatus.every(isStatusTrue);
-	const tracksPartiallySaved =
-		!allTracksSaved && tracksSavedStatus.some(isStatusTrue);
-	return {
-		allTracksSaved,
-		tracksPartiallySaved
-	};
+  const allTracksSaved = tracksSavedStatus.every(isStatusTrue);
+  const tracksPartiallySaved =
+    !allTracksSaved && tracksSavedStatus.some(isStatusTrue);
+  return {
+    allTracksSaved,
+    tracksPartiallySaved
+  };
 };
 
 const getAlbums = async (page = 1) => {
-	return await request({
-		url: "https://api.spotify.com/v1/me/albums",
-		params: { limit: ALBUMS_BATCH_SIZE, offset: ALBUMS_BATCH_SIZE * (page - 1) }
-	});
+  return await request({
+    url: "https://api.spotify.com/v1/me/albums",
+    params: { limit: ALBUMS_BATCH_SIZE, offset: ALBUMS_BATCH_SIZE * (page - 1) }
+  });
 };
 
 const removeTracks = async ids => {
-	return await request({
-		method: "delete",
-		url: "https://api.spotify.com/v1/me/tracks",
-		params: { ids: ids.join(",") }
-	});
+  return await request({
+    method: "delete",
+    url: "https://api.spotify.com/v1/me/tracks",
+    params: { ids: ids.join(",") }
+  });
 };
 
 const saveAlbum = async id => {
-	return await request({
-		method: "put",
-		url: "https://api.spotify.com/v1/me/albums",
-		params: { ids: id }
-	});
+  return await request({
+    method: "put",
+    url: "https://api.spotify.com/v1/me/albums",
+    params: { ids: id }
+  });
 };
 
 const migrateAlbum = async album => {
-	const { id, name, tracks, artists } = album;
-	const artistName = artists[0] ? artists[0].name : "Unknown";
-	const albumLabel = `"${name}" by ${artistName}`;
-	try {
-		const trackIds = tracks.items.map(item => item.id);
-		const { allTracksSaved, tracksPartiallySaved } = await checkTracksSaved(
-			trackIds
-		);
+  const { id, name, tracks, artists } = album;
+  const artistName = artists[0] ? artists[0].name : "Unknown";
+  const albumLabel = `"${name}" by ${artistName}`;
+  try {
+    const trackIds = tracks.items.map(item => item.id);
+    const { allTracksSaved, tracksPartiallySaved } = await checkTracksSaved(
+      trackIds
+    );
 
-		if (!allTracksSaved) {
-			console.log(
-				`Skipping ${albumLabel} as: ${
-					tracksPartiallySaved ? "Album partially saved" : "No tracks saved"
-				}`
-			);
-			return;
-		}
-		if (!DRY_RUN) {
-			await removeTracks(trackIds);
-			console.log(`Updated ${albumLabel}`);
-		} else {
-			console.log(`Would have updated ${albumLabel}`);
-		}
-		updatedAlbumIds.push(id);
-	} catch (e) {
-		console.log(`Error updating ${albumLabel}`, e);
-	}
+    if (!allTracksSaved) {
+      console.log(
+        `Skipping ${albumLabel} as: ${
+          tracksPartiallySaved ? "Album partially saved" : "No tracks saved"
+        }`
+      );
+      return;
+    }
+    if (!DRY_RUN) {
+      await removeTracks(trackIds);
+      console.log(`Updated ${albumLabel}`);
+    } else {
+      console.log(`Would have updated ${albumLabel}`);
+    }
+    updatedAlbumIds.push(id);
+  } catch (e) {
+    console.log(`Error updating ${albumLabel}`, e);
+  }
 };
 
 const migrateAlbumsInResults = async albumResultItems => {
-	for (albumItem of albumResultItems) {
-		await migrateAlbum(albumItem.album);
-	}
+  for (albumItem of albumResultItems) {
+    await migrateAlbum(albumItem.album);
+  }
 };
 
 const migrateAllAlbums = async () => {
-	let page = 1;
-	let hasNext = false;
-	try {
-		do {
-			const results = await getAlbums(page);
-			await migrateAlbumsInResults(results.items);
-			page++;
-			hasNext = results.next;
-		} while (hasNext && page < BATCH_LIMIT);
-	} catch (e) {
-		console.log(
-			`Unexpected Error in album batch #${page}. Abandoning further updates.`,
-			e
-		);
-	}
-	if (updatedAlbumIds.length > 0) {
-		console.log("Updated Album ids (just in case):\n", updatedAlbumIds);
-		console.log(
-			`\n\nUpdate completed: ${updatedAlbumIds.length} albums updated`
-		);
-	} else {
-		console.log("No albums updated");
-	}
+  let page = 1;
+  let hasNext = false;
+  try {
+    do {
+      const results = await getAlbums(page);
+      await migrateAlbumsInResults(results.items);
+      page++;
+      hasNext = results.next;
+    } while (hasNext && page < BATCH_LIMIT);
+  } catch (e) {
+    console.log(
+      `Unexpected Error in album batch #${page}. Abandoning further updates.`,
+      e
+    );
+  }
+  if (updatedAlbumIds.length > 0) {
+    console.log("Updated Album ids (just in case):\n", updatedAlbumIds);
+    console.log(
+      `\n\nUpdate completed: ${updatedAlbumIds.length} albums updated`
+    );
+  } else {
+    console.log("No albums updated");
+  }
 };
 
 migrateAllAlbums();
